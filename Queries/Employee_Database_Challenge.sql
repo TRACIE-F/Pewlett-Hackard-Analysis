8ec3f74eb4121cd427c5f8bb04824d37f4285de0
TRACIE-F <99625130+TRACIE-F@users.noreply.github.com>
2022-05-16 20:45:02 -0500

    Folder Reorganization

--- Queries/Employee_Database_Challenge.sql
+++ Queries/Employee_Database_Challenge.sql
@@ -1,4 +1,6 @@
--- Creating tables 
+-------------------
+-- CREATING TABLES
+-------------------
 CREATE TABLE departments (
      dept_no VARCHAR(4) NOT NULL,
      dept_name VARCHAR(40) NOT NULL,
@@ -36,13 +38,13 @@ CREATE TABLE salaries (
 );
 
 CREATE TABLE dept_emp (
-	dept_no VARCHAR NOT NULL,
 	emp_no INT NOT NULL,
+	dept_no VARCHAR NOT NULL,
     from_date DATE NOT NULL,
-    hire_date DATE NOT NULL,
+    to_date DATE NOT NULL,
 	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
     FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	PRIMARY KEY (dept_no)
+	PRIMARY KEY (emp_no, dept_no)
 );
 
 CREATE TABLE titles (
@@ -62,8 +64,9 @@ SELECT * FROM salaries;
 SELECT * FROM titles;
 SELECT * FROM employees;
 
-
--- Create empl_title data
+----------------------------------
+-- RETIREMENT DATA (DELIVERABLE 1)
+----------------------------------
 SELECT
 	e.emp_no,
 	e.first_name,
@@ -81,3 +84,76 @@ WHERE
 	and e.birth_date <= '1955-12-31'
 
 SELECT * FROM empl_title
+
+-- De-duplicate titles
+
+SELECT
+	distinct on (et.emp_no) et.emp_no,
+	et.first_name,
+	et.last_name,
+	et.title
+INTO
+	unique_titles
+FROM
+	empl_title et
+WHERE
+	et.to_date = '9999-01-01'
+ORDER BY
+	et.emp_no, et.to_date desc
+
+SELECT * FROM unique_titles
+
+-- Count the number of folks retiring soon
+
+SELECT
+	ut.title,
+	count(ut.emp_no) as title_count
+INTO
+	retiring_titles
+FROM
+	unique_titles ut
+GROUP BY
+	ut.title
+ORDER BY
+	title_count desc
+
+-- Print
+SELECT * FROM retiring_titles
+
+-----------------------------------------
+-- MENTORSHIP ELIGIBILITY (DELIVERABLE 2)
+-----------------------------------------
+SELECT
+	DISTINCT ON (e.emp_no) e.emp_no,
+	e.first_name,
+	e.last_name,
+	e.birth_date,
+	de.dept_no,
+	de.from_date,
+	de.to_date,
+	t.title
+INTO
+	mentorship_eligability
+FROM
+	employees e
+	JOIN dept_emp de on e.emp_no = de.emp_no
+	JOIN titles t on e.emp_no = t.emp_no
+WHERE
+	e.birth_date >= '1965-01-01'
+	AND e.birth_date <= '1965-12-31'
+	AND de.to_date = '9999-01-01'
+	AND t.to_date = '9999-01-01'
+ORDER BY
+	e.emp_no
+
+-- Print
+SELECT * FROM mentorship_eligability
+
+
+
+
+
+
+
+-- Code for table dropping
+DROP TABLE mentorship_eligability CASCADE;
